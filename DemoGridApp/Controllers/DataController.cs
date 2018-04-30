aad4d077abf50dd63feaebde31536711360023f7
nrifath2009 <nrifath2009@gmail.com>
2018-04-30 03:45:24 -0500

    Add project files.

---
+++ DemoGridApp/Controllers/DataController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DemoGridApp.Models;
+using Newtonsoft.Json;
+
+namespace DemoGridApp.Controllers
+{
+    public class DataController : Controller
+    {
+        public JsonResult Index(int?draw, int? start, int? length)
+        {
+            var search = Request["search[value]"];
+            var totalRecords = 0;
+            var totalRecordsFiltered = 0;
+            start = start.HasValue ? start / 10 : 0;
+
+            var products = new ProductRepository().GetPaginated(search, start.Value, length ?? 10, out totalRecords,
+                out totalRecordsFiltered);
+            //return JsonConvert.SerializeObject(products);
+            return Json(new { draw = draw, recordsFiltered = totalRecordsFiltered, recordsTotal = totalRecords, data = products });
+        }
+        
+    }
+}
---
+++ DemoGridApp/Models/ProductRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoGridApp.Models
+{
+    public class ProductRepository:IPagination<Product>
+    {
+        public static List<Product> GetAllProducts()
+        {
+            string[] rows = new[] {"IPhone","Smart TV","Laptop","Echo Plus","Android Phone","Digital Piano","Tablet"};
+            List<Product> products = new List<Product>();
+            for (int i = 1; i < 70; i++)
+            {
+                Product product = new Product();
+                product.Id = i;
+                product.ProductName = rows[i / 10]+" - "+i;
+                product.Price = i * 20;
+                product.Category = "Digital Product";
+                products.Add(product);
+            }
+            return products;
+        }
+
+        public IQueryable<Product> GetPaginated(string filter, int initialPage, int pageSize, out int totalRecords, out int recordsFiltered)
+        {
+            var data = GetAllProducts().AsQueryable();
+            totalRecords = data.Count();
+
+            if (!string.IsNullOrEmpty(filter))
+            {
+                data = data.Where(c => c.ProductName.ToUpper().Contains(filter.ToUpper()));
+            }
+            recordsFiltered = data.Count();
+            data = data
+                .OrderBy(c => c.Id)
+                .Skip((initialPage * pageSize))
+                .Take(pageSize);
+            return data;
+        }
+    }
+}
